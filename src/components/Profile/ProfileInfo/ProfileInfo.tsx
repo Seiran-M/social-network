8691736ec1732cb33240f1874fdc7646d7853ad1
Seiran-M <seyran.m1@gmail.com>
2021-04-23 04:29:30 +0300

    ProfileStatusWithHooks was added

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,6 +4,7 @@ import style from './ProfileInfo.module.css'
 import {Preloader} from '../../../common/Preloader/Preloader'
 import {ProfileType} from '../../../redux/profile-reducer'
 import {ProfileStatus} from './ProfileStatus'
+import {ProfileStatusWithHooks} from './ProfileStatusWithHooks'
 
 export const ProfileInfo: React.FC<PropsType> = (props) => {
    const {profile, status, updateUserStatus} = props
@@ -20,7 +21,7 @@ export const ProfileInfo: React.FC<PropsType> = (props) => {
             <div>About me: {profile.aboutMe}</div>
             <div>{profile.lookingForAJob}</div>
             <div>Looking a job: {profile.lookingForAJobDescription}</div>
-            <ProfileStatus status={status} updateUserStatus={updateUserStatus}/>
+            <ProfileStatusWithHooks status={status} updateUserStatus={updateUserStatus}/>
          </div>
       </>
    )
